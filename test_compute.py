cc9d2a61de70bd4eab1b8df4a4319480ac23e18f
unknown <elizaveta.genke@yandex.ru>
2018-02-23 11:47:02 +0100

    Change compute

--- test_compute.py
+++ test_compute.py
@@ -1,6 +1,7 @@
 from compute import divide
 from compute import multiply
 
+import numpy as np
 import pytest
 
 @pytest.mark.parametrize(
@@ -10,6 +11,11 @@ def test_divide(a, b, x):
     res = divide(a,b)
     print(x)
     assert res == pytest.approx(x) #better, because can occur some imprecisions due to type
+
+
+def test_divide_zero():
+    x = divide(2, 0)
+    assert np.isinf(x)
 
 def test_multiply():
     assert multiply(2,2)==4
